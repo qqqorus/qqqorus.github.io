49cc4198acca2389e18bd540905e71a65de71c71
Quishia Desiree Famador <httpsqorus@gmail.com>
2025-05-05 20:45:16 +0400

    update 28
    - implemented javascript to scroll the container using buttons
    - added a smooth scroll behavior property on the container element

---
+++ script.js
@@ -0,0 +1,9 @@
+function leftScroll() {
+    const left = document.querySelector(".flags-container");
+    left.scrollBy(-500, 0);
+}
+
+function rightScroll() {
+    const right = document.querySelector(".flags-container");
+    right.scrollBy(500, 0);
+}
