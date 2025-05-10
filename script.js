9c87a9f394570f04941c5e9bd1b8f504a69761cf
Quishia Desiree Famador <httpsqorus@gmail.com>
2025-05-10 17:41:58 +0400

    update 31
    edited the number that the container will scroll when the button is clicked

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function leftScroll() {
     const left = document.querySelector(".flags-container");
-    left.scrollBy(-510, 0);
+    left.scrollBy(-350, 0);
 }
 
 function rightScroll() {
     const right = document.querySelector(".flags-container");
-    right.scrollBy(510, 0);
+    right.scrollBy(350, 0);
 }
 
 let textDisplay = document.getElementsByClassName('flag-name');
