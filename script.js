8bf0428739ddc4f4b07cdcdf6dfa960fc315a706
Quishia Desiree Famador <httpsqorus@gmail.com>
2025-05-10 21:47:13 +0400

    update 39
    edited the scrollby value in js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function leftScroll() {
     const left = document.querySelector(".flags-container");
-    left.scrollBy(-350, 0);
+    left.scrollBy(-700, 0);
 }
 
 function rightScroll() {
     const right = document.querySelector(".flags-container");
-    right.scrollBy(350, 0);
+    right.scrollBy(700, 0);
 }
 
 let textDisplay = document.getElementsByClassName('flag-name');
